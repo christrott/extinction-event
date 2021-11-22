0570c339bc54c612ffaa2cbed613323ad4711307
Chris Trott <christrott86@gmail.com>
2021-11-22 15:18:34 +1100

    Added CardDirector and Card prefabs

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public Text titleText;
+    public Text contentText;
+
+    public void SetCardContent(string title, string content)
+    {
+        titleText.text = title;
+        contentText.text = content;
+    }
+}
---
+++ Assets/Scripts/CardDirector.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CardDirector : MonoBehaviour
+{
+    public GameObject cardPrefab;
+
+    private GameObject cardInstance;
+
+    // Show card at world position
+    public void ShowCardAtWorldPosition(Vector3 worldPosition, string title, string content)
+    {
+        if (cardInstance == null)
+        {
+            cardInstance = Instantiate(cardPrefab, gameObject.transform);
+        } else
+        {
+            cardInstance.SetActive(true);
+        }
+        var screenPosition = Camera.current.WorldToScreenPoint(worldPosition);
+        cardInstance.transform.position = screenPosition;
+        cardInstance.GetComponent<Card>().SetCardContent(title, content);
+        cardInstance.GetComponent<RectTransform>().pivot = getCardOffset(screenPosition, Camera.current);
+    }
+
+    // Show card at screen position
+    public void ShowCardAtScreenPosition(Vector2 screenPosition, string title, string content)
+    {
+        if (cardInstance == null)
+        {
+            cardInstance = Instantiate(cardPrefab, gameObject.transform);
+        }
+        else
+        {
+            cardInstance.SetActive(true);
+        }
+        cardInstance.transform.position = screenPosition;
+        cardInstance.GetComponent<Card>().SetCardContent(title, content);
+        cardInstance.GetComponent<RectTransform>().pivot = getCardOffset(screenPosition, Camera.current);
+    }
+
+    public void HideCard()
+    {
+        cardInstance.GetComponent<Card>().SetCardContent("", "");
+        cardInstance.SetActive(false);
+    }
+
+    private Vector2 getCardOffset(Vector2 screenPoint, Camera camera)
+    {
+        Vector2 screenDimensions = new Vector2(Camera.current.pixelWidth, Camera.current.pixelHeight);
+        return new Vector2(Mathf.Round(screenPoint.x / screenDimensions.x), Mathf.Round(screenPoint.y / screenDimensions.y));
+    }
+}
