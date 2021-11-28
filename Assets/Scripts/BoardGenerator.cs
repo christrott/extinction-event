5ed21c0db5570a6605cf771ddcde91d0866d0d7a
Chris Trott <christrott86@gmail.com>
2021-11-29 00:03:01 +1100

    Revamped generation algorithm

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -15,10 +15,12 @@ public class BoardGenerator : MonoBehaviour
     private const float alternateOffset = xSpacing / 2.0f;
     private GameObject board;
     private GameObject voidDirector;
+    private TileComponents tileComponents;
 
     // Start is called before the first frame update
     void Awake()
     {
+        tileComponents = GetComponent<TileComponents>();
         board = GameObject.Find("Board");
         voidDirector = GameObject.Find("VoidDirector");
         GenerateBoard(17, 17);
@@ -26,49 +28,50 @@ public class BoardGenerator : MonoBehaviour
 
     public void GenerateBoard(int xSize, int ySize)
     {
-        var tileIndex = 0;
         board.GetComponent<BoardManager>().tileSet = new Dictionary<string, GameObject>();
-        var tileComponents = GetComponent<TileComponents>();
         var playerPos = GeneratePlayerPosition(xSize, ySize);
         var voidPos = GenerateVoidPosition(xSize, ySize, playerPos);
         board.GetComponent<PlayerMoveController>().playerPos = playerPos;
         boardDimensions = new Vector2(xSize * xSpacing, ySize * ySpacing);
         Vector2 boardOffset = boardDimensions / 2;
 
-        for(int x = 0; x < xSize; x++)
+        for (int x = 0; x < xSize; x++)
         {
-            for(int y = 0; y < ySize; y++)
+            for (int y = 0; y < ySize; y++)
             {
                 var newTile = Instantiate(tilePrefab, board.transform);
-                newTile.GetComponent<TileInteractManager>().boardPosition = new Vector2(x, y);
                 string index = x + "," + y;
+                newTile.name = index;
+                newTile.GetComponent<TileInteractManager>().boardPosition = new Vector2(x, y);                
                 bool isOddRow = y % 2 == 1;
                 float xOffset = isOddRow ? boardOffset.x - alternateOffset : boardOffset.x;
                 newTile.transform.position = new Vector2(
                     x * xSpacing - xOffset,
                     y * ySpacing - boardOffset.y
                 );
-                if (playerPos.x == x && playerPos.y == y)
+                if (y == 0 || y == ySize - 1 || x == 0 || x == xSize - 1)
                 {
-                    newTile.GetComponent<TileEntityContainer>().AddEntity(startingPlayerEntity);
-                }
-                else if (voidPos.x == x && voidPos.y == y)
-                {
-                    newTile.GetComponent<TileEntityContainer>().AddEntity(voidFrontEntity);
-                    voidDirector.GetComponent<VoidController>().RegisterVoidFrontTile(newTile, false);
+                    Debug.Log("Place Exit tile at (" + x + "," + y + ")");
+                    var component = tileExitEntity;
+                    newTile.GetComponent<TileEntityContainer>().AddEntity(component);
                 }
                 else
                 {
-                    if (y == 0 || y == ySize - 1  || x == 0 || x == xSize - 1)
+                    var position = new Vector2(x, y);
+                    if (position == playerPos)
                     {
-                        Debug.Log("Place Exit tile at (" + x + "," + y + ")");
-                        var component = tileExitEntity;
-                        newTile.GetComponent<TileEntityContainer>().AddEntity(component);
-                    } else
+                        newTile.GetComponent<TileEntityContainer>().AddEntity(startingPlayerEntity);
+                    }
+                    else if (position == voidPos)
                     {
-                        tileIndex = (tileIndex + 1) % tileComponents.componentList.Count;
-                        var component = tileComponents.componentList[tileIndex];
-                        newTile.GetComponent<TileEntityContainer>().AddEntity(component);
+                        newTile.GetComponent<TileEntityContainer>().AddEntity(voidFrontEntity);
+                        voidDirector.GetComponent<VoidController>().RegisterVoidFrontTile(newTile, false);
+                    }
+                    else
+                    {
+                        int tier = getTierForPosition(position, new Vector2(xSize, ySize));
+                        TileEntity newEntity = getEntityForTier(tier, position);
+                        newTile.GetComponent<TileEntityContainer>().AddEntity(newEntity);
                     }
                 }
                 board.GetComponent<BoardManager>().tileSet[index] = newTile;
@@ -79,9 +82,11 @@ public class BoardGenerator : MonoBehaviour
 
     private Vector2 GeneratePlayerPosition(int xSize, int ySize)
     {
-        Vector2 xBand = new Vector2(Mathf.FloorToInt(xSize * 0.25f), Mathf.FloorToInt(xSize * 0.75f));
-        Vector2 yBand = new Vector2(Mathf.FloorToInt(ySize * 0.25f), Mathf.FloorToInt(ySize * 0.75f));
-        return new Vector2(xBand.x, yBand.x);
+        Vector2 xBand = new Vector2(Mathf.FloorToInt(xSize * 0.4f), Mathf.FloorToInt(xSize * 0.6f));
+        Vector2 yBand = new Vector2(Mathf.FloorToInt(ySize * 0.4f), Mathf.FloorToInt(ySize * 0.6f));
+        int randomX = Mathf.FloorToInt(Random.Range(xBand.x, xBand.y));
+        int randomY = Mathf.FloorToInt(Random.Range(yBand.x, yBand.y));
+        return new Vector2(randomX, randomY);
     }
 
     private Vector2 GenerateVoidPosition(int xSize, int ySize, Vector2 playerPosition)
@@ -89,5 +94,66 @@ public class BoardGenerator : MonoBehaviour
         Vector2 xBand = new Vector2(Mathf.FloorToInt(xSize * 0.25f), Mathf.FloorToInt(xSize * 0.75f));
         Vector2 yBand = new Vector2(Mathf.FloorToInt(ySize * 0.25f), Mathf.FloorToInt(ySize * 0.75f));
         return new Vector2(xBand.y, yBand.y);
+    }
+
+    private int getTierForPosition(Vector2 position, Vector2 mapSize)
+    {
+        var halfMapSize = mapSize / 2;
+        int xDistanceFromCentre = Mathf.FloorToInt(Mathf.Abs(halfMapSize.x - position.x));
+        int yDistanceFromCentre = Mathf.FloorToInt(Mathf.Abs(halfMapSize.y - position.y));
+
+        int xTier = Mathf.FloorToInt(xDistanceFromCentre / (halfMapSize.x / 3)) + 1;
+        int yTier = Mathf.FloorToInt(yDistanceFromCentre / (halfMapSize.y / 3)) + 1;
+        return xTier > yTier ? xTier : yTier;
+    }
+
+    private TileEntity getEntityForTier(int tier, Vector2 position)
+    {
+        var grassEntity = tileComponents.componentList[0];
+        var bushEntity = tileComponents.componentList[1];
+        var smallEntity = tileComponents.componentList[2];
+        var mediumEntity = tileComponents.componentList[3];
+        var largeEntity = tileComponents.componentList[4];
+
+        int random = Random.Range(0, 100);
+        if (random <= 80)
+        {
+            // Place tier appropriate entity
+            switch(tier)
+            {
+                case 1:
+                    return smallEntity;
+                case 2:
+                    return mediumEntity;
+                case 3:
+                    return largeEntity;
+            }
+        }
+        else if (random <= 90)
+        {
+            // Plants
+            if (random > 85)
+            {
+                return bushEntity;
+            }
+            else
+            {
+                return grassEntity;
+            }
+        }
+        else
+        {
+            // Other tier animal
+            switch (tier)
+            {
+                case 1:
+                    return mediumEntity;
+                case 2:
+                    return smallEntity;
+                case 3:
+                    return mediumEntity;
+            }
+        }
+        return grassEntity;
     }
 }
